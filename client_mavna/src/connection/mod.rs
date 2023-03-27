c9e5b316e3b5c5d27d5a82729b99e5063336c194
SEFA1905 <70607438+SEFA1905@users.noreply.github.com>
2023-03-27 03:21:15 +0300

    client requirements almost complete

--- client_mavna/src/connection/mod.rs
+++ client_mavna/src/connection/mod.rs
@@ -1,12 +1,14 @@
+
+
 use async_std::io::{ReadExt, WriteExt};
 #[warn(dead_code)]
 
 use async_std::{io::stdin, net::TcpStream, task};
-
+use libs::bar_stdin;
 
 pub async fn on_connection(){
     let stream = TcpStream::connect("127.0.0.1:8000").await;
-
+    
     match stream {
         
         Ok(mut stream)=>{
@@ -14,9 +16,16 @@ pub async fn on_connection(){
             
             println!("Bağlanma Başarili");
             task::spawn(async move {
-
-                handle_connection(stream).await;
-            });
+                let mut a = true;
+                
+                while a {
+                   match handle_connection(stream.clone()).await{
+                       Some(())=>continue,
+                       None => a = false
+                   } 
+                }
+                
+            }).await;
             
             
         },
@@ -25,22 +34,36 @@ pub async fn on_connection(){
     
 }
 
-pub async fn handle_connection(mut stream:TcpStream){
+pub async fn handle_connection(mut stream:TcpStream)->Option<()>{
     let mut buf = [0;1024];
+    println!("handle");
     match stream.read(&mut buf).await{
         Ok(0)=>{
             println!("Herhangi bir okuma yapilmadi");
-            println!("Ne soylemek istersiniz");
-            let mut str = String::new();
-            let bytes = stdin().read_line(&mut str).await;
-            stream.write(str.as_bytes()).await;
+
+            match bar_stdin::Stdin::read_line().await{
+                Some(value)=>{
+                    let bytes = value.user_message.as_bytes();
+                    stream.write(bytes).await;
+                    Some(())
+                },
+                None=>{
+                    println!("Kullanici Giriş Yapmadi Stream Kapatiliyor");
+                    None
+                }
+
+            }
+            
         }
         Ok(n)=>{
             println!("{} byte okundu",n);
             let str = String::from_utf8_lossy(&buf);
             println!("Gelen veri : ==>{}",str);
-            
+            Some(())
         }
-        Err(_) => todo!(),
+        Err(_) => {
+            println!("handle");
+            None
+            }
     }
 }
